a1adf84f5828a409aa63db67a672290633369cad
vinsbld <vincent.blndel@gmail.com>
2019-02-06 15:50:23 +0100

    une sauvegarde de méthode

--- Dn_Ex_5/src/com/company/Main.java
+++ Dn_Ex_5/src/com/company/Main.java
@@ -6,18 +6,22 @@ public class Main {
 
 
         int[] tabFizz = {3, 6, 9, 12, 15, 18, 21};
-        //le fizz doit revoyer les multiples de 3
+        //le fizz doit renvoyer les multiples de 3
+        int fizz = 3;
 
-        int calKul;
-        calKul = tabFizz.length/3;
-
-
-
-
+//premiere boucle fait le tour du tableau
         for (int i = 0; i <= tabFizz.length; i++) {
-            if (3/tabFizz[i]==1){//est un multiple de 3 donc = 3/n == 1,2,3,4,5) {
-                System.out.println("la valeur à la position " + i + " du tableau correspondand au chiffre" + tabFizz[i] + " est un multiple de 3");
-            }
+            //pour chaque valeur[i] du tableau divisée par 3 sur tout le tableau j est égale à toutes les valeurs divisées par trois
+            for (int j = fizz / tabFizz[i]; j <= tabFizz.length; i++) {
+                //if (3/tabFizz[i]==1){//est un multiple de 3 donc = 3/n == 1,2,3,4,5)
+                for (int k= 0;fizz<=tabFizz[j];k++){
+                //si 3 divisées par chaque index est égale 1* lui même ou 2* lui même etc
+                if (tabFizz[j]==) {
+                    System.out.println("la valeur à la position " + i + " du tableau correspondand au chiffre" + tabFizz[i] + " est un multiple de 3");
+                }
+                }
+                }
+        }
             else{
                 System.out.println("ce n'en est pas un");
             }

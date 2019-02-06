9bd3d049c9050f466396cb3e6d03953e9a9f3855
vinsbld <vincent.blndel@gmail.com>
2019-02-06 17:51:04 +0100

    fais du propre

--- Dn_Ex_5/src/com/company/Main.java
+++ Dn_Ex_5/src/com/company/Main.java
@@ -6,10 +6,9 @@ public class Main {
 
 
         int[] tabFizz = {3, 6, 9, 12, 15, 18, 21};
-        //le fizz doit revoyer les multiples de 3
-int div;
 
-        //premiere boucle fait le tour du tableau
+int div;
+        // boucle fait le tour du tableaule fizz doit revoyer les multiples de 3
         for (div = 3 / tabFizz.length; div< tabFizz.length; div++) {
 
                     System.out.println("la valeur à la position " + div + " du tableau correspondand au chiffre " + tabFizz[div] + " est un multiple de 3");
@@ -17,5 +16,5 @@ int div;
 
             }
             }
-        
+
 

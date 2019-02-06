5c072e183dbf69890740cf82b7cd0a2e36858d7d
vinsbld <vincent.blndel@gmail.com>
2019-02-06 17:42:30 +0100

    ça run pour les multiples de 3

--- Dn_Ex_5/src/com/company/Main.java
+++ Dn_Ex_5/src/com/company/Main.java
@@ -7,21 +7,15 @@ public class Main {
 
         int[] tabFizz = {3, 6, 9, 12, 15, 18, 21};
         //le fizz doit revoyer les multiples de 3
-
-
+int div;
 
         //premiere boucle fait le tour du tableau
-        for (int i = 0 ; i<= tabFizz.length; i++) {
-            int div = 3 / tabFizz[i];
-            int resultatDeDiv = div;
-            resultatDeDiv++;
+        for (div = 3 / tabFizz.length; div< tabFizz.length; div++) {
 
-            //si la valeur dans l'index i divisée par 3  = 1  resultatDeDiv++;
-            while (div <tabFizz.length) {
-                    System.out.println("la valeur à la position " + i + " du tableau correspondand au chiffre " + tabFizz[i] + " est un multiple de 3");
+                    System.out.println("la valeur à la position " + div + " du tableau correspondand au chiffre " + tabFizz[div] + " est un multiple de 3");
                 }
 
             }
             }
-        }
+        
 

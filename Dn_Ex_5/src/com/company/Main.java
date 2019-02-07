a22dd38688cfb5b0d0f01a94840e1af0d28af628
vinsbld <vincent.blndel@gmail.com>
2019-02-07 10:13:27 +0100

    work in progress

--- Dn_Ex_5/src/com/company/Main.java
+++ Dn_Ex_5/src/com/company/Main.java
@@ -8,10 +8,15 @@ public class Main {
         int[] tabFizz = {3, 6, 9, 12, 15, 18, 21};
 
 int div;
+int i;
         // boucle fait le tour du tableaule fizz doit revoyer les multiples de 3
-        for (div = 3 / tabFizz.length; div< tabFizz.length; div++) {
+        for (i=0; i< tabFizz.length; i++) {
+            //div doit diviser tous les nombres du tableau par lui même
+            div = 3/tabFizz[i];
+            //le résultat de div doit être un entier 1,2,3,4* lui même
+            //si le résultat est un entier il affiche la phrase
 
-                    System.out.println("la valeur à la position " + div + " du tableau correspondand au chiffre " + tabFizz[div] + " est un multiple de 3");
+                    System.out.println("la valeur à la position " + i + " du tableau correspondand au chiffre " + tabFizz[i] + " est un multiple de 3");
                 }
 
             }

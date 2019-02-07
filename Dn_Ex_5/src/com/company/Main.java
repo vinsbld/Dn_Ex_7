b53b4fc57d4ded9aaf7b29cc8150b05ffc7f6a71
vinsbld <vincent.blndel@gmail.com>
2019-02-07 15:37:42 +0100

    Exercice 5 done !

--- Dn_Ex_5/src/com/company/Main.java
+++ Dn_Ex_5/src/com/company/Main.java
@@ -1,25 +1,45 @@
 package com.company;
 
+import java.time.Year;
+
 public class Main {
 
     public static void main(String[] args) {
 
 
-        int[] tabFizz = {3, 6, 9, 12, 15, 18, 21};
+        int[] tabFizz = {3,5, 15,9, 8,13,10,1};
 
-int div;
+int div3;
+int div5;
 int i;
-        // boucle fait le tour du tableaule fizz doit revoyer les multiples de 3
+        // boucle fait le tour du tableau
         for (i=0; i< tabFizz.length; i++) {
-            //div doit diviser tous les nombres du tableau par lui même
-            div = 3/tabFizz[i];
-            //le résultat de div doit être un entier 1,2,3,4* lui même
-            //si le résultat est un entier il affiche la phrase
 
-                    System.out.println("la valeur à la position " + i + " du tableau correspondand au chiffre " + tabFizz[i] + " est un multiple de 3");
+            //div doit diviser tous les nombres du tableau par lui même
+            div3 = tabFizz[i]%3;
+div5=tabFizz[i]%5;
+
+//si il n'y a pas de résidus à div 3
+            if (div3 == 0){
+                System.out.println("Fizz");
+            }
+
+            //si il n'y a pas de résidus à div 5
+            else if (div5 == 0){
+                System.out.println("Buzz");
+            }
+//si div3 et div 5 retournent 0
+            else if (div3 ==0 && div5==0){
+                System.out.println("FizzBuzz");
+            }
+            //sinon return "Nul"
+            else{
+                System.out.println("Null");
+            }
+
                 }
 
             }
-            }
+          }
 
 

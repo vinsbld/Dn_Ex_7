6889695d3013cd33db89be69f56cb22e3f9cb813
vinsbld <vincent.blndel@gmail.com>
2019-02-11 11:26:09 +0100

    premier push

---
+++ Dn_Ex_7/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Ecrire une fonction à laquelle si on passe une phrase elle retourne le mot le plus long de la phrase.
+    }
+}
